f188adcf33b2da36b0f8ca671c1db34faadd7caf
someoddperson <pawel.a.rybak@gmail.com>
2018-12-09 13:59:52 +0100

    Initial Commit

---
+++ finding_pepsi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  finding_pepsi
+//
+//  Created by Paweł Rybak on 09/12/2018.
+//  Copyright © 2018 Paweł Rybak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
